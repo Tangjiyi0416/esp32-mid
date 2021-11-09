2678b930e32fc808e3c6d7582c5a62a423a3d04e
Tang jiyi <37148354+Tangjiyi0416@users.noreply.github.com>
2021-11-09 11:53:36 +0800

    Add files via upload

---
+++ mid.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+Servo myservo;
+const int servoPin = 13;
+int state = 0;
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(servoPin);
+  Serial.begin(115200);
+}
+const int inputPin = 12;
+void loop() {
+  // put your main code here, to run repeatedly:
+  int p_time = 0;
+  int dist_cm = 0;
+  pinMode(inputPin, OUTPUT);
+  digitalWrite(inputPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(inputPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(inputPin, LOW);
+  pinMode(inputPin, INPUT);
+  p_time = pulseIn(inputPin, HIGH); //unit: us
+  dist_cm = (float)((p_time / 2) / 29.41);
+  if (dist_cm <= 5 && state != 1) {
+    myservo.write(180);
+    delay(20);
+    state = 1;
+  }
+  else if (dist_cm > 10 && state != 0) {
+    myservo.write(0);
+    delay(20);
+    state = 0;
+  }
+  Serial.print(state);
+  Serial.print(" ");
+  Serial.println(dist_cm);
+  delay(120);
+  //  myservo.write(180);
+}
